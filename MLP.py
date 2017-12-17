8763093410c91acb3394a23240897f1c9b68242d
jaypengfei <469844540@qq.com>
2017-12-17 22:10:32 +0800

    mlp
    a multilayer perceptrons

---
+++ MLP.py
@@ -0,0 +1,39 @@
+import tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+sess = tf.InteractiveSession()
+#784 = 28*28
+in_units = 784
+h1_units = 300
+#w1 b1 for hiddden w2 b2 for output
+w1 = tf.Variable(tf.truncated_normal([in_units, h1_units], stddev = 0.1))
+b1 = tf.Variable(tf.zeros([h1_units]))
+w2 = tf.Variable(tf.zeros([h1_units, 10]))
+b2 = tf.Variable(tf.zeros([10]))
+
+#x: input keep_prob:rate of dropout
+x = tf.placeholder(tf.float32, [None, in_units])
+keep_prob = tf.placeholder(tf.float32)
+
+#define struct of module
+#hidden
+hidden1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
+#output layer
+y = tf.nn.softmax(tf.matmul(hidden1_drop, w2) + b2)
+#real label
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices = [1]))
+train_step = tf.train.AdagradOptimizer(0.3).minimize(cross_entropy)
+
+#training
+tf.initialize_all_variables().run()
+for i in range(3000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	#input x, y_ keep_prob
+	train_step.run({x: batch_xs, y_:batch_ys, keep_prob: 0.75})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
